dfc6a873de6a74d9209b77d0eee106fafcef80b5
Andrew Nelson <nelsand19@gmail.com>
2024-03-07 18:59:49 -0700

    implement an api and start displaying it on screen

---
+++ script.js
@@ -0,0 +1,20 @@
+const div = document.querySelector("#weather")
+
+
+function getData () {
+axios.get('https://api.api-ninjas.com/v1/weather?city=pocatello', {
+    headers: {
+        'X-Api-Key': 'EECScCOXhR8nxF19K2ElnA==RYF3uhKbpK4QkvOz'
+    }
+}).then ((res) => {
+    
+    console.log(res.data)
+    
+    div.textContent = res.data.temp
+}).catch ((err) => {
+    console.log(err.message)
+})
+
+}
+
+getData()
