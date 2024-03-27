e084c8c188519c864a515c5fe08ee93bfeef7982
Andrew Nelson <nelsand19@gmail.com>
2024-03-26 20:55:33 -0600

    add api to page

--- script.js
+++ script.js
@@ -1,20 +1,44 @@
-const div = document.querySelector("#weather")
+const weatherDiv = document.querySelector(".weatherData")
+const weatherForm = document.querySelector("form")
+const weatherInput = document.querySelector(".weatherSearch")
 
 
-function getData () {
-axios.get('https://api.api-ninjas.com/v1/weather?city=pocatello', {
+
+function handleForm (event) {  
+event.preventDefault();
+
+const searchValue = weatherInput.value;
+weatherInput.value = '';
+
+if (!searchValue) {
+    weatherDiv.textContent = 'Please select a city!';
+    weatherDiv.style.color = 'red';
+    return
+}
+
+
+axios.get(`https://api.api-ninjas.com/v1/weather?city=${searchValue}`, {
     headers: {
         'X-Api-Key': 'EECScCOXhR8nxF19K2ElnA==RYF3uhKbpK4QkvOz'
     }
 }).then ((res) => {
-    
-    console.log(res.data)
-    
-    
+    console.log(res.data)                                                                                                      
+    weatherDiv.innerHTML = `
+    <div class="resultsContainer">
+      <p class="weatherResults">The current temperature is ${res.data.temp}C</p>
+      <P class="maxTemp">Max temp is ${res.data.max_temp}C</p>
+      <p class="minTemp">Min temp is ${res.data.min_temp}C</p>
+      <p class="feelsLike">Feels like ${res.data.feels_like}C</p>
+    </div>
+    `
+    throw {message: 'Error!'}
 }).catch ((err) => {
     console.log(err.message)
+    weatherDiv.textContent = 'Something went wrong, please try again later!'
 })
 
 }
 
-getData()
+
+
+weatherForm.addEventListener('submit', handleForm)
